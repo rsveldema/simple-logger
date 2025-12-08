a9a46eede7f9622de0a0ec6ea6eafb016bf180ff
Ronald Veldema <Ronald.Veldema@sennheiser.com>
2025-12-08 11:03:44 +0100

    feat: Formatter support for error code

--- include/slogger/Error.hpp
+++ include/slogger/Error.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <format>
+#include <string>
+
 /**
  * @file Error.hpp
  * @brief Defines error codes and conversion functions for network operations.
@@ -7,7 +10,7 @@
 
 namespace error
 {
-    
+
 enum class Error
 {
     OK,
@@ -23,5 +26,40 @@ enum class Error
 };
 
 Error errno_to_error(int err);
+} // namespace error
 
-}
+
+template <> struct std::formatter<error::Error>
+{
+    constexpr auto parse(std::format_parse_context& ctx)
+    {
+        return ctx.begin();
+    }
+
+    auto format(const error::Error& c, std::format_context& ctx) const
+    {
+        switch (c) {
+        case error::Error::OK:
+            return std::format_to(ctx.out(), "OK");
+        case error::Error::RANGE:
+            return std::format_to(ctx.out(), "RANGE");
+        case error::Error::MMAP_FAILED:
+            return std::format_to(ctx.out(), "MMAP_FAILED");
+        case error::Error::FAILED_TO_CREATE_SOCKET:
+            return std::format_to(ctx.out(), "FAILED_TO_CREATE_SOCKET");
+        case error::Error::FAILED_TO_OPEN_PCM:
+            return std::format_to(ctx.out(), "FAILED_TO_OPEN_PCM");
+        case error::Error::ALSA_FAILURE:
+            return std::format_to(ctx.out(), "ALSA_FAILURE");
+        case error::Error::NO_ALSA_CAPTURE:
+            return std::format_to(ctx.out(), "NO_ALSA_CAPTURE");
+        case error::Error::NO_ALSA_PLAYBACK:
+            return std::format_to(ctx.out(), "NO_ALSA_PLAYBACK");
+        case error::Error::HOSTNAME_RESOLVE_FAILED:
+            return std::format_to(ctx.out(), "HOSTNAME_RESOLVE_FAILED");
+        case error::Error::UNKNOWN:
+            return std::format_to(ctx.out(), "UNKNOWN");
+        }
+        return std::format_to(ctx.out(), "UNKNOWN_ERROR_CODE");
+    }
+};
